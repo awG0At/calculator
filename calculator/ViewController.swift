a7c0f2550094ce4800f7feb5e6771e42172a27dd
aw <89423@psdschools.org>
2022-08-29 10:53:21 -0600

    "to string" is way harder to pull off here than it is with visual scripting nodes

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var calculatedOutput: UILabel!
+    @IBOutlet weak var userText: UITextField!
+    @IBOutlet weak var userText2: UITextField!
+    
+
+    @IBOutlet weak var easterImage: UIImageView!
+    
+    public var num1:Float = 0
+    public var num2:Float = 0
+    public var output:Float = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func buttonActive(_ sender: UIButton) {
+        var tofloat = (userText.text as! NSString).floatValue
+        num1 = tofloat
+        var tofloat2 = (userText2.text as! NSString).floatValue
+        num2 = tofloat2
+        print(num1)
+        print(num2)
+      
+        output = num1 * num2
+        
+        print(output)
+        
+        calculatedOutput.text = "\(output)"
+        
+        
+        
+        if output == 64
+        {
+            easterImage.image = UIImage(named:"mario64")
+        }
+    }
+    
 
 }
 
