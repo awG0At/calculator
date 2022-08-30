9d71f086c3ae37cdd4b3217d40c7fd5d70521f95
aw <89423@psdschools.org>
2022-08-30 10:49:12 -0600

    mult and dev

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -12,37 +12,59 @@ class ViewController: UIViewController {
     @IBOutlet weak var calculatedOutput: UILabel!
     @IBOutlet weak var userText: UITextField!
     @IBOutlet weak var userText2: UITextField!
-    
-
     @IBOutlet weak var easterImage: UIImageView!
     
     public var num1:Float = 0
     public var num2:Float = 0
     public var output:Float = 0
-    
+    public var mult:Int = 0
+    public var dev:Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func devide(_ sender: UIButton) {
+        dev = 1
+        mult = 0
+    }
+    
+    @IBAction func multi(_ sender: UIButton) {
+        dev = 0
+        mult = 1
+    }
+    
+    
     @IBAction func buttonActive(_ sender: UIButton) {
-        var tofloat = (userText.text as! NSString).floatValue
-        num1 = tofloat
-        var tofloat2 = (userText2.text as! NSString).floatValue
-        num2 = tofloat2
-        print(num1)
-        print(num2)
-      
-        output = num1 * num2
-        
-        print(output)
-        
-        calculatedOutput.text = "\(output)"
-        
+
+            var tofloat = (userText.text as! NSString).floatValue
+            num1 = tofloat
+            var tofloat2 = (userText2.text as! NSString).floatValue
+            num2 = tofloat2
+            print(num1)
+            print(num2)
+            
+        if dev == 1 {
+            output = num1 / num2
+            print(output)
+            calculatedOutput.text = "\(output)"
+        }
+        else{
+            output = num1 * num2
+            print(output)
+            calculatedOutput.text = "\(output)"
+        }
+           
+    
         
         
         if output == 64
         {
+            easterImage.isHidden = false
             easterImage.image = UIImage(named:"mario64")
+        }
+        else{
+            easterImage.isHidden = true
         }
     }
     
